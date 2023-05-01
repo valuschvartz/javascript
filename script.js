fa040c8d8bc62e653e097d39dfe749caca3af8fa
valuschvartz <valentinaschvartz@gmail.com>
2023-05-01 00:52:29 -0300

    Segunda preentrega

--- script.js
+++ script.js
@@ -1,63 +1,165 @@
-let carrito = [];
-function myFunction(producto) {
-    let cantidad = prompt(`¿Cuántos ${producto.nombre} desea agregar al carrito?`);
-    let confirmacion = confirm(`¿Estás seguro que quieres agregar ${cantidad} ${producto.nombre} al carrito?`);
-  
-    if (confirmacion) {
-      // Si el usuario confirma que quiere agregar la respuesta al carrito
-      let subtotal = producto.precio * cantidad ;
-      let productoAgregado = {
-        nombre: producto.nombre,
-        precio: producto.precio,
-        cantidad: cantidad,
-        subtotal: subtotal,
-      };
-      carrito.push(productoAgregado);
-      let alerta = alert(`Se han agregado ${cantidad} ${producto.nombre} al carrito, su subtotal es ${subtotal}`);
-    } else {
-      // Si el usuario no confirma que quiere agregar la respuesta al carrito
-      let alerta = alert("No se ha agregado nada al carrito");
+
+const contenedorServicios = document.getElementById('contenedor-servicios')
+
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const botonVaciar = document.getElementById('vaciar-carrito')
+
+const btnEnviar= document.getElementById('btnEnviar')
+
+
+
+const confirmar= document.getElementById('confirmar')
+
+const aceptar=document.getElementById('aceptar')
+
+
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
+
+let carrito = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
     }
-  }
-  function mostrarCarrito(subtotal){
-    let mensaje = "Carrito de compras:\n";
-    let total = 0;
-  for (let i = 0; i < carrito.length; i++) {
-    mensaje += `Producto: ${carrito[i].nombre}, Precio: ${carrito[i].precio}, Cantidad: ${carrito[i].cantidad}, Subtotal: ${carrito[i].subtotal}\n`;
-    total += carrito [i].subtotal;
-  }
-  mensaje += `Total: ${total}`;
-  let resumen = alert(mensaje);
+})
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
+
+Servicios.forEach((servicio) => {
+    const div = document.createElement('div')
+    div.classList.add('servicio') 
+    div.innerHTML = `
+    <img src=${servicio.img} alt= "">
+    <h3>${servicio.nombre}</h3>
+    <p class="precioServicio">Precio: $${servicio.precio}</p>
+    <button id="agregar${servicio.id}" class="boton-agregar">Agregar</button>`;
+
+    contenedorServicios.appendChild(div)
+
+    
+    const boton = document.getElementById(`agregar${servicio.id}`)
+   
+
+    boton.addEventListener('click', () => {
+       
+        agregarAlCarrito(servicio.id)
+
+        swal("¿Desea agregarlo a sus compra?", {
+            buttons: {
+              cancel: "Cancelar",
+              catch: {
+                text: "Aceptar",
+                value: "catch",
+              },
+            },
+          })
+          .then((value) => {
+            switch (value) {
+           
+              case "catch":
+                swal({
+                    text: "Se ha agregado a su compra",
+                    button: "Ok",
+                  })
+                break;
+           
+            }
+          });
+        })
+        
+        })
+
+
+        
+aceptar.addEventListener('click', () => {
+
+swal({
+        title: "Su compra fue confirmada",
+        icon: "success",
+        button: "Ok",
+      })
+    })
+
+
+const agregarAlCarrito = (servId) => {
+
+    
+    const existe = carrito.some (serv => serv.id === servId) 
+
+    if (existe){ 
+        const serv = carrito.map (serv => { 
+           
+            if (serv.id === servId){
+                serv.cantidad++
+            }
+        })
+    } else { 
+        const item = Servicios.find((serv) => serv.id === servId)
+        
+        carrito.push(item)
+        
+    }
+    
+    actualizarCarrito() 
+    
+    console.log (carrito)
 }
-  function vaciarCarrito() {
-    let vaciar = confirm("Estas seguro que deseas vaciar el carrito?")
-    if (vaciar) {
-      // Si el usuario confirma que quiere vaciar el carrito
-      carrito.splice(0, carrito.length);
-     let alerta = alert("Se ha vaciado el carrito");
-    } else {
-      // Si el usuario no confirma que quiere vaciar el carrito
-      let alerta = alert("No se ha vaciado el carrito");
-    }
-  }
-  function confirmarCompra() {
-    let mensaje = "Carrito de compras:\n";
-    let total = 0;
-    for (let i = 0; i < carrito.length; i++) {
-        mensaje += `Producto: ${carrito[i].nombre}, Precio: ${carrito[i].precio}, Cantidad: ${carrito[i].cantidad}, Subtotal: ${carrito[i].subtotal}\n`;
-        total += carrito [i].subtotal;
-    }
-    mensaje += `Total: ${total}`;
 
-    let comprar = confirm(`¿Desea confirmar su compra?\n\n${mensaje}`);
-    if (comprar) {
-        // El usuario confirmó la compra, se vacía el carrito
-        carrito.splice(0, carrito.length);
-        let alerta = alert("Gracias por su compra");
-    } else {
-        // El usuario canceló la compra
-        let alerta = alert("Compra cancelada");
-    }
-  }
+
+
+      
+
+
+const eliminarDelCarrito = (servId) => {
+    const item = carrito.find((serv) => serv.id === servId)
+
+    const indice = carrito.indexOf(item) 
+
+    carrito.splice(indice, 1)  
+   
+    actualizarCarrito() 
+   
+    console.log(carrito)
+}
+
+const actualizarCarrito = () => {
+    
+    contenedorCarrito.innerHTML = "" 
   
+    carrito.forEach((serv) => {
+        const div = document.createElement('div')
+        div.className = ('servicioEnCarrito')
+        div.innerHTML = `
+        <p>${serv.nombre}</p>
+        <p>Precio: $${serv.precio}</p>
+        <button onclick="eliminarDelCarrito(${serv.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
 
+        contenedorCarrito.appendChild(div)
+        
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    })
+    
+    precioTotal.innerText = carrito.reduce((acc, serv) => acc + serv.cantidad * serv.precio, 0)
+    
+
+}
+
+AOS.init({
+    offset: 100, 
+    delay: 0, 
+    duration: 1000 
+  });
+
+
+
+
+ 
+ 
